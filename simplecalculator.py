6e5b5ae11d3e94668de577e67a100d31b5fe73e8
Nimraakhann <141023796+Nimraakhann@users.noreply.github.com>
2024-07-13 14:35:34 +0500

    Update simplecalculator.py

--- simplecalculator.py
+++ simplecalculator.py
@@ -1,14 +1,17 @@
 a = int(input("Enter first number: "))
 b = int(input("Enter second number: "))
-sign = input("Enter the operation to be performed: ")
-if sign == "+" :
-  print("the result is: ", a+b)
-elif sign == "-" :
-  print("the result is: ", a-b)
-elif sign == "*" :
-  print("the result is: ", a*b)
-elif sign == "/" :
-  if b == 0 :
-    print("Error: Division by zero is not allowed.")
-  else :
-    print("the result is: ",a/b)
+sign = input("Enter the operation to be performed: [+-*/] ")
+while sign not in "+-*/" :
+  sign = input("Enter the correct operation to be performed: [+-*/] ")
+else:  
+ if sign == "+" :
+   print("the result is: ", a+b)
+ elif sign == "-" :
+   print("the result is: ", a-b)
+ elif sign == "*" :
+   print("the result is: ", a*b)
+ elif sign == "/" :
+   if b == 0 :
+     print("Error: Division by zero is not allowed.")
+   else :
+     print("the result is: ",a/b)
