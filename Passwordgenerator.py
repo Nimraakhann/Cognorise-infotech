0b1362bf85ce8b43ac4e3225fb8df30ebfe7eb39
Nimraakhann <141023796+Nimraakhann@users.noreply.github.com>
2024-07-13 14:32:49 +0500

    Update Passwordgenerator.py

--- Passwordgenerator.py
+++ Passwordgenerator.py
@@ -2,8 +2,9 @@ import random
 import string
 
 len = int(input("Enter the length of password: "))
-if len < 6 :
-  print("Length of Password should be greater than 6")
+while len < 6 :
+  
+  len = int(input("Length of Password should be greater than 6: "))
 else:
    characters = string.ascii_letters + string.digits + string.punctuation
    lst = [random.choice(characters) for i in range(len)] 
